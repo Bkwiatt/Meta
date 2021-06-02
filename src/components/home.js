d632dc993a2d5b74b16451ff29364f233c09ffc0
Bkwiatt <bkwiat7@gmail.com>
2021-06-02 15:14:01 -0500

    Setup project

---
+++ src/components/home.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const home = () => {
+    return (
+        <>
+            <Container>
+
+            </Container>
+        </>
+    )
+}
+
+export default home
+
+
+const Container = styled.div`
+    height: 100vh;
+    background-image: url('ghostBG.jpeg');
+`
---
+++ src/components/navbar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const navbar = () => {
+    
+    return (
+        <>
+            <NavContainer>
+                
+            </NavContainer>
+            
+        </>
+    )
+}
+
+export default navbar
+
+
+const NavContainer = styled.div`
+    background-color: black;
+    height: 100px;
+`
